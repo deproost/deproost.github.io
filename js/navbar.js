6bf7a1345ac58129e4f0f33f222d887c33e300f4
Senne Deproost <senne.deproost@siemens.com>
2023-08-02 14:35:45 +0200

    Timeline

--- js/navbar.js
+++ js/navbar.js
@@ -1,25 +1,25 @@
 // Script for side navigation
 function w3_open() {
-    var x = document.getElementById("sidebar");
-    x.style.width = "300px";
-    x.style.paddingTop = "10%";
-    x.style.display = "block";
+  var x = document.getElementById("sidebar");
+  x.style.width = "300px";
+  x.style.paddingTop = "10%";
+  x.style.display = "block";
+}
+
+// Close side navigation
+function w3_close() {
+  document.getElementById("sidebar").style.display = "none";
+}
+
+// Used to toggle the menu on smaller screens when clicking on the menu button
+function openNav(anchor) {
+  var x = document.getElementById("sidenav");
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+  } else {
+    x.className = x.className.replace(" w3-show", "");
   }
-  
-  // Close side navigation
-  function w3_close() {
-    document.getElementById("sidebar").style.display = "none";
-  }
-  
-  // Used to toggle the menu on smaller screens when clicking on the menu button
-  function openNav(anchor) {
-    var x = document.getElementById("sidenav");
-    if (x.className.indexOf("w3-show") == -1) {
-      x.className += " w3-show";
-    } else { 
-      x.className = x.className.replace(" w3-show", "");
-    }
-    var icon = anchor.querySelector("i");
-    icon.classList.toggle('fa-bars');
-    icon.classList.toggle('fa-close');
-  }
+  var icon = anchor.querySelector("i");
+  icon.classList.toggle('fa-bars');
+  icon.classList.toggle('fa-close');
+}
